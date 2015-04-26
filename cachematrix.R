3e11e4ad2271557c8ac7da32e40a6a339c957fe1
Anna Nikulasdottir <anna@skerpa.com>
2015-04-26 21:33:54 +0200

    changes for the completion of assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions allow the user to cache the inverse of a matrix,
+## in order to save costly computation when the inverse of the same matrix is
+## needed repeatedly, e.g. in a loop.
+## Note that the functions do not contain tests for the invertibility of the 
+## matrix supplied, it is assumed, that it is always invertible!
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## It creates a list of functions for setting a new matrix (and deleting the 
+## cached inverse), getting the matrix, setting and getting the inverse matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    ## assigns y to x in the parent environment
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inv <<- solve
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix 'x'. If the inverse for 'x'
+## was already computed, returns the cached inverse, otherwise computes the 
+## inverse and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
